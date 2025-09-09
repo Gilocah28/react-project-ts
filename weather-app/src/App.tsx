12d94d0b972c2685b6aa31fd0ace95f0d42295d5
Gilocah28 <gberxx@gmail.com>
2025-09-09 16:53:45 +0800

    fetching data

--- weather-app/src/App.tsx
+++ weather-app/src/App.tsx
@@ -1,4 +1,24 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+
 const App = () => {
+  const [weatherData, setWeatherData] = useState<any>(null);
+
+  const dataKey: string = import.meta.env.VITE_WEATHER_KEY;
+
+  const getWeatherData = async () => {
+    const response = await axios.get(
+      `http://api.weatherapi.com/v1/forecast.json?key=${dataKey}&q=Malasiqui&days=7&aqi=yes&alerts=no`
+    );
+    setWeatherData(response.data);
+  };
+
+  useEffect(() => {
+    getWeatherData();
+  }, []);
+  
+  console.log(weatherData ? weatherData.location.name : "no data");
+
   return <div>App</div>;
 };
 
